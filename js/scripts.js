7ba96d923fb89f05820ba728d522b4ce3954d2e3
LaTanya Donaldson and Zach Bastian <zach@>
2017-03-22 15:54:39 -0700

    it will add form inputs.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $("#food").submit(function(event){
+  event.preventDefault();
+
+  var add = function("#food");
+
+  var flavor = $("input:radio[name=flavor]:checked").val();
+
+  var dob = $("#born").val();
+
+  var favoriteColor = $("#color").val();
+
+
+
+
+
+  };
+});
